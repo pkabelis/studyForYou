c8b0adb34d470276d570e3528673c78bd71f3cb6
pkabe <pkabe9@gmail.com>
2020-09-04 17:40:13 +0200

    init commit

---
+++ src/com/jsf2leaf/model/Descriptions.java
@@ -0,0 +1,33 @@
+
+package com.jsf2leaf.model;
+
+public class Descriptions {
+	
+	public static final String BITWA_POD_KROJANTAMI = "Bitwa pod Krojantami, Szarża pod Krojantami, Krojanty (1 września 1939) – szarża kawalerii polskiej w trakcie kampanii wrześniowej." +
+			"1 września 18 pułk ułanów (dowódca – pułkownik Kazimierz Mastalerz) ze składu Pomorskiej Brygady Kawalerii osłaniał odwrót wojsk Grupy Operacyjnej „Czersk” z armii „Pomorze”." +
+			" W godzinach popołudniowych w rejonie miejscowości Krojanty 2 szwadrony pułku wykonały szarżę na nacierające oddziały piechoty z niemieckiej 20 Dywizji Zmotoryzowanej.";
+
+	public static final String OBRONA_HELU = "Obrona Helu, bitwa o Hel (1 września–2 października 1939) – działania obronne oddziałów Armii polskiej w Rejonie Umocnionym Helu." +
+			"W czasie kampanii wrześniowej półwysep, jako tak zwany Rejon Umocniony Helu, broniony był przez około 3 tysiące żołnierzy pod dowództwem komandora Włodzimierza Steyera. " +
+			"Początkowo Niemcy ograniczali się do bombardowania Helu z powietrza i morza. 8 września rozpoczęli także ataki lądowe. 12 września przełamali polskie linie obronne u nasady półwyspu w rejonie Swarzewa oraz Wielkiej Wsi i izolowali go od reszty wybrzeża. ";
+
+	public static final String OBRONA_WESTERPLATTE = "Obrona Westerplatte, oblężenie Westerplatte (1–7 września 1939) – obrona Wojskowej Składnicy Tranzytowej w trakcie kampanii wrześniowej." +
+			"Od 1925 roku na terenie Wolnego Miasta Gdańska, na niewielkim półwyspie u wejścia do portu znajdowała się polska Wojskowa Składnica Tranzytowa, w 1939 roku obsadzona przez 182 żołnierzy (w tym 5 oficerów i 1 lekarza) pod dowództwem majora Henryka Sucharskiego.";
+
+	public static final String BITWA_POD_MLAWA = "Bitwa pod Mławą, Mława (1–4 września 1939) – bitwa obronna stoczona w toku kampanii wrześniowej pomiędzy oddziałami 20 Dywizji Piechoty, " +
+			"wchodzącej w skład armii „Modlin”, a nacierającymi jednostkami 1 Korpusu Armijnego oraz Korpusu Armijnego „Wodrig”, wchodzącymi w skład 3 Armii niemieckiej (dowódca – generał Georg von Küchler).";
+
+	public static final String BITWA_POD_BZURA = "Bitwa nad Bzurą, Bitwa pod Kutnem, Kutno (9–18 września 1939) – największa bitwa kampanii wrześniowej, stoczona na Równinie Kutnowskiej, na zachód od Warszawy, " +
+			"przez połączone siły armii „Poznań” i „Pomorze” pod ogólnym dowództwem generała Stanisława Kutrzeby z jednostkami 8 i 10 Armii niemieckiej wchodzącymi w skład Grupy Armii „Süd” (dowódca – generał pułkownik Gerd von Rundstedt).";
+
+	public static final String OBRONA_WARSZAWY = "Obrona Warszawy, Warszawa (8–28 września 1939) – bitwa powietrzno-lądowa stoczona w obronie stolicy w toku kampanii wrześniowej 1939 roku." +
+			"8 września jednostki XVI Korpusu Pancernego 10 Armii niemieckiej dotarły od południowego zachodu do podwarszawskich miejscowości Grójec, Raszyn, Piaseczno i Pruszków, a w godzinach popołudniowych " +
+			"usiłowały zająć stolicę z marszu, atakując pozycje obrońców na przedmieściach Ochoty. Garnizon Warszawy pod dowództwem generała brygady Waleriana Czumy odparł ten atak, podobnie jak i ataki 4 Dywizji Pancernej w dniu następnym, zadając przeciwnikowi poważne straty.";
+
+	public static final String OBRONA_TWIERDZY_BRZESKIEJ = "Obrona twierdzy brzeskiej, obrona twierdzy Brześć (14–17 września 1939) – w czasie kampanii wrześniowej twierdza była broniona przez 3 bataliony piechoty pod dowództwem generała brygady Konstantego Plisowskiego przed nacierającymi " +
+			"oddziałami niemieckiego XIX Korpusu Pancernego (dowódca – generał Heinz Guderian). Po dwudniowych atakach, prowadzonych przez 10 Dywizję Pancerną oraz 20 Dywizję Zmotoryzowaną, twierdza padła.";
+
+	public static final String BITWA_POD_PIOTRKOWEM = "Pierwsza Bitwa pod Tomaszowem Lubelskim, Tomaszów Lubelski (17–20 września 1939) – pierwsza z dwóch bitew stoczonych w tym rejonie w trakcie kampanii wrześniowej." +
+			"Okrążone na południowy zachód od Tomaszowa Lubelskiego resztki Armii „Kraków” (6, 23, 55 Dywizja Piechoty, Krakowska Brygada Kawalerii) oraz „Lublin” (Warszawska Brygada Pancerno-Motorowa, grupa „Sandomierz”) pod ogólnym dowództwem generała " +
+			"dywizji Tadeusza Piskora podjęły 17 września wieczorem próbę przebicia się do Lwowa, uderzając na pozycje niemieckiego XXII Korpusu Pancernego (dowódca – generał Ewald von Kleist) w rejonie Tomaszowa Lubelskiego–Bełżec–Narol.";
+}
